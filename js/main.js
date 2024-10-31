d8caeab135426dab2a49e143572706fda4746729
jdbcamilo <149972010+jdbcamilo@users.noreply.github.com>
2024-10-31 13:03:54 -0500

    oaa probando 5

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const navLinks = document.querySelectorAll('nav a');
     const menuToggle = document.getElementById('menu-toggle');
     const nav = document.querySelector('nav');
     
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            navLinks.forEach(l => l.classList.remove('active'));
-            this.classList.add('active');
-            if (window.innerWidth <= 600) {
-                nav.classList.remove('show');
-            }
-        });
-    });
-
-    menuToggle.addEventListener('click', function() {
+    menuToggle.addEventListener('click', () => {
         nav.classList.toggle('show');
     });
 
-    // Añadir clase para animar el contenido principal
-    const mainContent = document.querySelector('main');
-    mainContent.classList.add('animate-content');
+    // Cerrar el menú al hacer clic en un enlace
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('show');
+        });
+    });
 
-    // Cerrar menú al hacer clic fuera de él
-    document.addEventListener('click', function(event) {
+    // Cerrar el menú al hacer clic fuera de él
+    document.addEventListener('click', (event) => {
         const isClickInsideNav = nav.contains(event.target);
         const isClickOnMenuToggle = menuToggle.contains(event.target);
         if (!isClickInsideNav && !isClickOnMenuToggle && nav.classList.contains('show')) {
             nav.classList.remove('show');
         }
     });
+
+    // Añadir clase para animar el contenido principal
+    const mainContent = document.querySelector('main');
+    mainContent.classList.add('animate-content');
 });
